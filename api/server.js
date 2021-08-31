4037ee7a93505078b0c4789feff8b5961d8a41ee
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-08-31 13:57:28 -0400

    set up server

--- api/server.js
+++ api/server.js
@@ -1,2 +1,12 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require("express");
+const server = express();
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(404).json({ message: "Page not found" });
+});
+
+module.exports = server;
